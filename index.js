703878c8d362ae83b2f5589ad0f2a2a992e06966
Paul B. <paulr@bump.sh>
2024-03-26 15:35:09 +0100

    style: use 'standard' eslint rules to have a consistent code style
    This commit adds eslint library with the standard ruleset and applies
    all rules to the existing codebase.
    
    This will help have a unique code style accross the repository.

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 #!/usr/bin/env node
-import arg from 'arg';
-import {overlayFiles} from './src/overlay.js'
+import arg from 'arg'
+import { overlayFiles } from './src/overlay.js'
 
-function showHelp() {
-    console.log("Usage: overlayjs --openapi FILEPATH --overlay FILEPATH");
-    console.log("    use --help to see this help");
+function showHelp () {
+  console.log('Usage: overlayjs --openapi FILEPATH --overlay FILEPATH')
+  console.log('    use --help to see this help')
 }
 
 try {
-    const args = arg({
-        '--openapi': String,
-        '--overlay': String,
-        '--help': String
-    });
+  const args = arg({
+    '--openapi': String,
+    '--overlay': String,
+    '--help': String
+  })
 
-    if(args['--overlay'] && args['--openapi']) {
-        const openapiFile = args['--openapi'];
-        const overlayFile = args['--overlay'];
-        var spec = overlayFiles(openapiFile, overlayFile);
-        console.log(spec);
-    } else {
-        showHelp()
-    }
-
+  if (args['--overlay'] && args['--openapi']) {
+    const openapiFile = args['--openapi']
+    const overlayFile = args['--overlay']
+    const spec = overlayFiles(openapiFile, overlayFile)
+    console.log(spec)
+  } else {
+    showHelp()
+  }
 } catch (err) {
-    if (err.code === 'ARG_UNKNOWN_OPTION') {
-        console.warn(err.message);
-        showHelp()
-    } else {
-        throw err;
-    }
+  if (err.code === 'ARG_UNKNOWN_OPTION') {
+    console.warn(err.message)
+    showHelp()
+  } else {
+    throw err
+  }
 }
-
